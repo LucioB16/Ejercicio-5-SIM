2725e66bb94d5c56fdca99f0980ab237e86aaca1
LucioB16 <38698067+LucioB16@users.noreply.github.com>
2025-08-03 21:49:11 -0300

    Reorganizar UI y ejes de gráficos

--- SimulacionCmiWPF/GraficosViewModel.cs
+++ SimulacionCmiWPF/GraficosViewModel.cs
@@ -9,12 +9,27 @@ namespace SimulacionCmiWPF;
 /// </summary>
 public class GraficosViewModel
 {
+    /// <summary>Número de visita para cada punto.</summary>
+    public double[] Visitas { get; }
+
+    /// <summary>Ventas acumuladas por visita.</summary>
     public double[] VentasAcumuladas { get; }
+
+    /// <summary>Probabilidad acumulada de respuesta "definitivamente sí".</summary>
     public double[] ProbabilidadSi { get; }
+
+    /// <summary>Primer índice de visita mostrado.</summary>
+    public double PrimerVisita { get; }
+
+    /// <summary>Último índice de visita mostrado.</summary>
+    public double UltimaVisita { get; }
 
     public GraficosViewModel(IList<VectorEstado> vectores)
     {
+        Visitas = vectores.Select(v => (double)v.Visita).ToArray();
         VentasAcumuladas = vectores.Select(v => (double)v.VentasAcum).ToArray();
         ProbabilidadSi = vectores.Select(v => v.ProbAcumSi).ToArray();
+        PrimerVisita = Visitas.First();
+        UltimaVisita = Visitas.Last();
     }
 }
--- SimulacionCmiWPF/VentanaGraficos.xaml.cs
+++ SimulacionCmiWPF/VentanaGraficos.xaml.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Windows;
 using ScottPlot;
@@ -20,13 +21,23 @@ public partial class VentanaGraficos : Window
         if (DataContext is not GraficosViewModel vm)
             return;
 
-        double[] xs = Enumerable.Range(1, vm.VentasAcumuladas.Length).Select(i => (double)i).ToArray();
+        double[] xs = vm.Visitas;
+
+        // Ventas acumuladas
         plotVentas.Plot.Add.Scatter(xs, vm.VentasAcumuladas);
         plotVentas.Plot.Title("Ventas acumuladas");
+        plotVentas.Plot.Axes.SetLimitsX(vm.PrimerVisita, vm.UltimaVisita);
         plotVentas.Refresh();
 
-        plotProbabilidad.Plot.Add.Scatter(xs, vm.ProbabilidadSi);
+        // Probabilidad acumulada de "definitivamente sí" en escala logarítmica
+        double[] logProb = vm.ProbabilidadSi.Select(p => p > 0 ? Math.Log10(p) : double.NegativeInfinity).ToArray();
+        plotProbabilidad.Plot.Add.Scatter(xs, logProb);
         plotProbabilidad.Plot.Title("P(Def. Sí)");
+        plotProbabilidad.Plot.Axes.SetLimitsX(vm.PrimerVisita, vm.UltimaVisita);
+        plotProbabilidad.Plot.Axes.SetLimitsY(-4, 0);
+        plotProbabilidad.Plot.Axes.Left.TickGenerator = new ScottPlot.TickGenerators.NumericManual(
+            new double[] { -4, -3, -2, -1, 0 },
+            new string[] { "≈0", "0,001", "0,01", "0,1", "1" });
         plotProbabilidad.Refresh();
     }
 }
