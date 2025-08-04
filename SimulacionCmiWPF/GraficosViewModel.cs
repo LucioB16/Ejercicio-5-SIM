442962a87dce1432d8c57715c8b532b08f54e787
LucioB16 <38698067+LucioB16@users.noreply.github.com>
2025-08-03 21:48:59 -0300

    Reorganiza UI y ajusta gráficos

--- SimulacionCmiWPF/GraficosViewModel.cs
+++ SimulacionCmiWPF/GraficosViewModel.cs
@@ -11,10 +11,17 @@ public class GraficosViewModel
 {
     public double[] VentasAcumuladas { get; }
     public double[] ProbabilidadSi { get; }
+    public int PrimeraVisita { get; }
+    public int UltimaVisita { get; }
 
     public GraficosViewModel(IList<VectorEstado> vectores)
     {
         VentasAcumuladas = vectores.Select(v => (double)v.VentasAcum).ToArray();
         ProbabilidadSi = vectores.Select(v => v.ProbAcumSi).ToArray();
+        if (vectores.Count > 0)
+        {
+            PrimeraVisita = vectores[0].Visita;
+            UltimaVisita = vectores[^1].Visita;
+        }
     }
 }
--- SimulacionCmiWPF/VentanaGraficos.xaml.cs
+++ SimulacionCmiWPF/VentanaGraficos.xaml.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Windows;
 using ScottPlot;
@@ -20,13 +21,28 @@ public partial class VentanaGraficos : Window
         if (DataContext is not GraficosViewModel vm)
             return;
 
-        double[] xs = Enumerable.Range(1, vm.VentasAcumuladas.Length).Select(i => (double)i).ToArray();
+        double[] xs = Enumerable.Range(vm.PrimeraVisita, vm.VentasAcumuladas.Length)
+            .Select(i => (double)i).ToArray();
+
         plotVentas.Plot.Add.Scatter(xs, vm.VentasAcumuladas);
         plotVentas.Plot.Title("Ventas acumuladas");
+        plotVentas.Plot.Axes.SetLimits(vm.PrimeraVisita, vm.UltimaVisita);
         plotVentas.Refresh();
 
-        plotProbabilidad.Plot.Add.Scatter(xs, vm.ProbabilidadSi);
+        double[] logProb = vm.ProbabilidadSi
+            .Select(p => Math.Log10(Math.Max(p, 1e-3)))
+            .ToArray();
+        plotProbabilidad.Plot.Add.Scatter(xs, logProb);
         plotProbabilidad.Plot.Title("P(Def. Sí)");
+        plotProbabilidad.Plot.Axes.SetLimits(vm.PrimeraVisita, vm.UltimaVisita, -3, 0);
+
+        var ticks = new ScottPlot.TickGenerators.NumericAutomatic
+        {
+            LabelFormatter = v => Math.Pow(10, v).ToString("0.###"),
+            MinorTickGenerator = new ScottPlot.TickGenerators.EvenlySpacedMinorTickGenerator(0.05),
+            MaxTickCount = 4
+        };
+        plotProbabilidad.Plot.Axes.Left.TickGenerator = ticks;
         plotProbabilidad.Refresh();
     }
 }
